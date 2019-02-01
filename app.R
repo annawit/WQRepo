0172a3fedb5480d3381922abbb646734b58cd869
WITHINGTON Anna <awithin@deq.state.or.us>
2019-02-01 12:27:07 -0800

    Major updates to layout, tabs

--- app.R
+++ app.R
@@ -14,9 +14,10 @@ load("dataforwqapp.Rdata")
 # stations$Lat <- as.numeric(stations$Lat_DD)
 # stations$Long <- as.numeric(stations$Long_DD)
 
-md2 <- dta1 %>%
-  filter(!is.na(datetime)) %>% 
-  mutate(Site = paste(MLocID, StationDes))
+md2 <- dta1
+# md2 <- dta1 %>%
+#   filter(!is.na(datetime)) %>% 
+#   mutate(Site = paste(MLocID, StationDes))
 
 #colors
 coul <- colorRampPalette(brewer.pal(9, "Set3"))(14)
@@ -25,135 +26,147 @@ coul <- colorRampPalette(brewer.pal(9, "Set3"))(14)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   navbarPage(
-    "A Shiny App for Dissolved Oxygen",
-
-# Main Tab 1 --------------------------------------------------------------
-tabPanel("Select from map",
-         tabsetPanel(
-           
-
-# Subpanel 1.1 ------------------------------------------------------------
-           tabPanel("Table",
-                    sidebarLayout(
-                      sidebarPanel(
-                        leafletOutput("map"),
-                        textOutput("temp"),
-                        checkboxGroupButtons(
-                          inputId = "checkboxtablesorter",
-                          label = "Select table columns to display:",
-                          choices = names(md2),
-                          selected = c("MLocID", "datetime", "temp", "ph",
-                                       "do", "cond", "data_source",
-                                       "StationDes")
-                          ),
-                        HTML("Select a station and variables,
+    "Continuous Dissolved Oxygen Visualizer",
+    
+    # Main Tab 1 --------------------------------------------------------------
+    tabPanel("Select from map",
+             tabsetPanel(
+               
+               
+               # Subpanel 1.1 ------------------------------------------------------------
+               tabPanel("Table",
+                        sidebarLayout(
+                          sidebarPanel(
+                            leafletOutput("map"),
+                            textOutput("temp"),
+                            checkboxGroupButtons(
+                              inputId = "checkboxtablesorter",
+                              label = "Select table columns to display:",
+                              choices = names(md2),
+                              selected = c("MLocID", "datetime", "temp", "ph",
+                                           "do", "cond", "data_source",
+                                           "StationDes")
+                            ),
+                            HTML("Select a station and variables,
                              then hit 'Download data'."),
-                        br(), br(),
-                        downloadButton(
-                          outputId = "download_data",
-                          label = "Download table data")
-                      ),
-                      mainPanel(DT::dataTableOutput("table"))
-                      )
-                    ),
-
-# Subpanel 1.2 ------------------------------------------------------------
-tabPanel("Plot of total number of samples per year",
-         plotlyOutput("nplot"),
-         plotlyOutput("avgdoplot")),
-
-# Subpanel 1.3 ------------------------------------------------------------
-
-tabPanel("Other overview visualizations",
-         plotlyOutput("summaryboxplot"),
-         plotlyOutput("summaryboxplot2"),
-         plotlyOutput("summaryboxplot3"))
-         )),
-
-# Main Tab 2 --------------------------------------------------------------
-
-tabPanel(
-  "Search by Station",
-  fluidPage(
-    tabsetPanel(
-      tabPanel("Plots",
-               sidebarPanel(
-                 selectInput(
-                   inputId = "station_selection",
-                   label = "Select station:",
-                   choices = md2$Site
-                 ),
-                 selectInput(
-                   inputId = "x",
-                   label = "x-axis",
-                   choices = names(md2),
-                   selected = "datetime"
-                 ),
-                 # selectInput(
-                 #   inputId = "y",
-                 #   label = "y-axis",
-                 #   choices = names(md2),
-                 #   selected = "do"
-                 # ),
-                 selectInput(
-                   inputId = "y2",
-                   label = "2nd y-axis",
-                   choices = names(md2),
-                   selected = "temp"
-                 ),
-                 selectInput(
-                   inputId = "y3",
-                   label = "3rd y-axis",
-                   choices = names(md2),
-                   selected = "cond"
-                 ),
-                 selectInput(
-                   inputId = "y4",
-                   label = "4th y-axis",
-                   choices = names(md2),
-                   selected = "ph"
-                 )
+                            br(), br(),
+                            downloadButton(
+                              outputId = "download_data",
+                              label = "Download table data")
+                          ),
+                          mainPanel(DT::dataTableOutput("table"))
+                        )
                ),
-               mainPanel(
-                 textOutput("tab2text"),
-                 plotlyOutput("subplot"),
-                 verbatimTextOutput("brush")
-               )),
-      tabPanel("Data Selected From Graph, in a Table",
-               DT::dataTableOutput("graph_to_table"))
+               
+               # Subpanel 1.2 ------------------------------------------------------------
+               tabPanel("Placeholder"),
+               
+               # Subpanel 1.3 ------------------------------------------------------------
+               
+               tabPanel("Placeholder2"),
+             )),
+    
+    # Main Tab 2 --------------------------------------------------------------
+    tabPanel("Data Overview",
+             # Subpanel Samples per year ------------------------------------------------------------
+             tabsetPanel(
+               tabPanel("Plot of total number of samples per year",
+                        plotlyOutput("nplot")),
+               tabPanel("DO at each site",
+                        plotlyOutput("avgdoplot")),
+               
+               # Subpanel boxplots ------------------------------------------------------------
+               
+               tabPanel("Other overview visualizations",
+                        plotlyOutput("summaryboxplot"),
+                        plotlyOutput("summaryboxplot2"),
+                        plotlyOutput("summaryboxplot3"))
+             )),
+    
+    # Main Tab 3 --------------------------------------------------------------
+    
+    tabPanel(
+      "Search by Station",
+      fluidPage(
+        tabsetPanel(
+          tabPanel("Plots",
+                   sidebarPanel(
+                     selectInput(
+                       inputId = "station_selection",
+                       label = "Select station:",
+                       choices = md2$Site
+                     ),
+                     selectInput(
+                       inputId = "x",
+                       label = "x-axis",
+                       choices = names(md2),
+                       selected = "datetime"
+                     ),
+                     # selectInput(
+                     #   inputId = "y",
+                     #   label = "y-axis",
+                     #   choices = names(md2),
+                     #   selected = "do"
+                     # ),
+                     selectInput(
+                       inputId = "y2",
+                       label = "2nd y-axis",
+                       choices = names(md2),
+                       selected = "temp"
+                     ),
+                     selectInput(
+                       inputId = "y3",
+                       label = "3rd y-axis",
+                       choices = names(md2),
+                       selected = "cond"
+                     ),
+                     selectInput(
+                       inputId = "y4",
+                       label = "4th y-axis",
+                       choices = names(md2),
+                       selected = "ph"
+                     )
+                   ),
+                   mainPanel(
+                     textOutput("tab2text"),
+                     plotlyOutput("subplot"),
+                     verbatimTextOutput("brush")
+                   )),
+          tabPanel("Data Selected From Graph, in a Table",
+                   DT::dataTableOutput("graph_to_table"))
+        )
       )
-    )
-  ),
-
-# Main Tab 3 --------------------------------------------------------------
-
-tabPanel("Search by DO pass/fail",
-         fluidPage(
-           sidebarLayout(
-             sidebarPanel(
-               selectizeInput(
-                 inputId = "station_pf",
-                 label = "Select station(s):",
-                 choices = levels(md2$MLocID),
-                 multiple = TRUE
-               ),
-               radioButtons(
-                 inputId = "DOPassFailRadio",
-                 label = "Select:",
-                 choices = c("Pass", "Fail", "All"),
-                 selected = "All"
-               )
-             ),
-             mainPanel(
-               tabsetPanel(
-                 tabPanel("Map"),
-                 tabPanel("Table")
+    ),
+    
+    # Main Tab 4 --------------------------------------------------------------
+    
+    tabPanel("Search by DO over/under limit",
+             fluidPage(
+               sidebarLayout(
+                 sidebarPanel(
+                   selectizeInput(
+                     inputId = "station_pf",
+                     label = "Select station(s):",
+                     choices = levels(md2$MLocID),
+                     multiple = TRUE
+                   ),
+                   radioButtons(
+                     inputId = "DOPassFailRadio",
+                     label = "Select:",
+                     choices = c("Pass", "Fail", "All"),
+                     selected = "All"
+                   )
+                 ),
+                 mainPanel(
+                   tabsetPanel(
+                     tabPanel("Map"),
+                     tabPanel("Table")
+                   )
                  )
                )
              )
-           )
-         )
-)
+    )
+  )
 ) # End Fluid Page
 
 # Server ------------------------------------------------------------------
@@ -203,13 +216,15 @@ server <- function(input, output, session) {
       filter(MLocID == input$map_marker_click$id)
     DT::datatable(
       data = maptabledata,
-      options = list(pageLength = 10),
+      options = list(pageLength = 10,
+                     scrollX = TRUE),
       rownames = FALSE,
       filter = 'bottom'
-    )
+    ) %>% 
+      DT::formatDate("datetime", "toLocaleString")
   })
-  
-  # output$dateplot <- renderPlot()
+
+# Subpanel 1.1 items ------------------------------------------------------
   output$nplot <- renderPlotly({
     md2 %>% 
       group_by(month = floor_date(datetime, "month")) %>% 
@@ -217,7 +232,7 @@ server <- function(input, output, session) {
       mutate(season = factor(month.abb[month(datetime)],
                              levels = c("May", "Jun", "Jul", "Aug", "Oct"))) %>% 
       plot_ly(x = ~month, y = ~n_samples, type = "bar",
-              color = ~season, colors = viridis_pal()(3)) %>% 
+              color = ~season, colors = viridis_pal()(3)) %>%
       layout(yaxis = list(title = 'Count'),
              xaxis = list(title = "Date",
                           range = c("2007-01-01", "2016-12-31")),
@@ -229,9 +244,10 @@ server <- function(input, output, session) {
       group_by(MLocID, month.p = floor_date(datetime, "month")) %>% 
       mutate(min = min(do))
     
-    plot_ly(wdi, x = ~month.p, y = ~min, color = ~Site,
-            colors = coul,
+    plot_ly(wdi, x = ~month.p, y = ~min,
             text = ~paste('Site:', StationDes)) %>%
+      add_markers(color = ~Site,
+                  colors = coul) %>% 
       layout(xaxis = list(title = "Date",
                           range = c("2007-01-01", "2016-12-31")),
              yaxis = list(title = "Minimum Dissolved Oxygen"))
@@ -277,7 +293,7 @@ server <- function(input, output, session) {
   })
   
   
-  # Main Tab 2 Items --------------------------------------------------------
+  # Main Tab 3 Items --------------------------------------------------------
   
   output$tab2text <- renderUI({
     str1 <- paste("You have selected", input$station_selection)
